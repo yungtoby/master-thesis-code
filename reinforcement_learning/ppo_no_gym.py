1898ca5381a1edb279991790d183068d1fbde34b
Tobias Husebø <tooobbbi.urmaker@gmail.com>
2026-02-07 14:00:57 +0100

    started batched env

--- reinforcement_learning/ppo_no_gym.py
+++ reinforcement_learning/ppo_no_gym.py
@@ -1,97 +1,62 @@
 #########################################################################################################################
 # THE FOLLOWING CODE (LINE 5 - 316) IS FROM CLEANRL. URL: https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/ppo.py #
-# DISCLAIMER: MINOR CHANGES HAS BEEN MADE TO FIT THESIS PROJECT.                                                        #
+# DISCLAIMER: MAJOR CHANGES HAS BEEN MADE TO FIT THESIS PROJECT.                                                        #
 #########################################################################################################################
-
 # docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ppo/#ppopy
+
+################### QUICK FIX FOR IMPORTS: ############################################ 
+import os, sys                                                                        #
+print(f'NAME: --- {os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}')     #
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))          #
+#######################################################################################
+
+# IMPORTS
 import os
 import random
 import time
-from dataclasses import dataclass
-
-import gymnasium as gym
+import tyro
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import tyro
+import gymnasium as gym
+from dataclasses import dataclass
 from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
-
-#FIXME: quick fix for imports
-import os, sys
-print(f'NAME: --- {os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}')
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-#My imports
 from reinforcement_learning.envs.BO_gym_env import BOEnv
 from reinforcement_learning.factories import GPFactory, CandidateFactory
 from functions.blackbox_function import BlackBoxFunc
 from functions.own_functions import not_too_easy_unique_opt
 
-
-@dataclass
-class Args:
-    exp_name: str = os.path.basename(__file__)[: -len(".py")]
-    """the name of this experiment"""
-    seed: int = 1
-    """seed of the experiment"""
-    torch_deterministic: bool = True
-    """if toggled, `torch.backends.cudnn.deterministic=False`"""
-    cuda: bool = True
-    """if toggled, cuda will be enabled by default"""
-    track: bool = False
-    """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
-    """the wandb's project name"""
-    wandb_entity: str = None
-    """the entity (team) of wandb's project"""
-    capture_video: bool = False
-    """whether to capture videos of the agent performances (check out `videos` folder)"""
-
-    # Algorithm specific arguments
-    env_id: str = "RL-BO-v0.1"
-    """the id of the environment"""
-    total_timesteps: int = 10000 #TODO: UP THE COUNT
-    """total timesteps of the experiments"""
-    learning_rate: float = 2.5e-4
-    """the learning rate of the optimizer"""
-    num_envs: int = 50 #TODO: UP NUMBER OF PARALLEL 
-    """the number of parallel game environments"""
-    num_steps: int = 128
-    """the number of steps to run in each environment per policy rollout"""
-    anneal_lr: bool = True
-    """Toggle learning rate annealing for policy and value networks"""
-    gamma: float = 0.99
-    """the discount factor gamma"""
-    gae_lambda: float = 0.95
-    """the lambda for the general advantage estimation"""
-    num_minibatches: int = 4
-    """the number of mini-batches"""
-    update_epochs: int = 4
-    """the K epochs to update the policy"""
-    norm_adv: bool = True
-    """Toggles advantages normalization"""
-    clip_coef: float = 0.2
-    """the surrogate clipping coefficient"""
-    clip_vloss: bool = True
-    """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
-    ent_coef: float = 0.01
-    """coefficient of the entropy"""
-    vf_coef: float = 0.5
-    """coefficient of the value function"""
-    max_grad_norm: float = 0.5
-    """the maximum norm for the gradient clipping"""
-    target_kl: float = None
-    """the target KL divergence threshold"""
-
-    # to be filled in runtime
-    batch_size: int = 0
-    """the batch size (computed in runtime)"""
-    minibatch_size: int = 0
-    """the mini-batch size (computed in runtime)"""
-    num_iterations: int = 0
-    """the number of iterations (computed in runtime)"""
+# CURRENT PLACEHOLDER FOR ARGS COMMAND USED BY CLEANRL
+args = {
+    'exp_name' : os.path.basename(__file__)[: -len(".py")],
+    'seed' : 1,
+    'torch_deterministic' : True,
+    'cuda' : True,
+    'track' : False,
+    'capture_video' : False,
+    'env_id' : "RL-BO-v0.1",
+    'total_timesteps' : 500000,
+    'learning_rate' : 2.5e-4,
+    'num_envs' : 4,
+    'num_steps' : 128,
+    'anneal_lr' : True,
+    'gamma' : 0.99,
+    'gae_lambda' : 0.95,
+    'num_minibatches' : 4,
+    'update_epochs' : 4,
+    'norm_adv' : True,
+    'clip_coef' : 0.2,
+    'clip_vloss' : True,
+    'ent_coef' : 0.01,
+    'vf_coef' : 0.5,
+    'max_grad_norm' : 0.5,
+    'target_kl' : None,
+    'batch_size': 0,
+    'minibatch_size' : 0,
+    'num_iterations' : 0
+}
 
 
 def make_env():
@@ -135,8 +100,10 @@ class Agent(nn.Module):
             layer_init(nn.Linear(64, envs.single_action_space.n), std=0.01),
         )
 
+
     def get_value(self, x):
         return self.critic(x)
+
 
     def get_action_and_value(self, x, action=None):
         logits = self.actor(x)
@@ -146,71 +113,64 @@ class Agent(nn.Module):
         return action, probs.log_prob(action), probs.entropy(), self.critic(x)
 
 
-if __name__ == "__main__":
-    args = tyro.cli(Args)
-    args.batch_size = int(args.num_envs * args.num_steps)
-    args.minibatch_size = int(args.batch_size // args.num_minibatches)
-    args.num_iterations = args.total_timesteps // args.batch_size
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-    if args.track:
-        import wandb
 
-        wandb.init(
-            project=args.wandb_project_name,
-            entity=args.wandb_entity,
-            sync_tensorboard=True,
-            config=vars(args),
-            name=run_name,
-            monitor_gym=True,
-            save_code=True,
-        )
-    writer = SummaryWriter(f"runs/{run_name}")
+
+
+if __name__ == "__main__":
+    args['batch_size'] = int(args['num_envs'] * args['num_steps'])
+    args['minibatch_size'] = int(args['batch_size'] // args['num_minibatches'])
+    args['num_iterations'] = args['total_timesteps'] // args['batch_size']
+
+    run_name = f"{args['env_id']}__{args['exp_name']}__{args['seed']}__{int(time.time())}"
+
+    writer = SummaryWriter(f'runs/{run_name}')
     writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+        'hyperparameters',
+        '|param|value|\n|-|-|\n%s' % ('\n'.join([f'|{key}|{value}|' for key, value in vars(args).items()])),
     )
 
     # TRY NOT TO MODIFY: seeding
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = args.torch_deterministic
+    random.seed(args['seed'])
+    np.random.seed(args['seed'])
+    torch.manual_seed(args['seed'])
+    torch.backends.cudnn.deterministic = args['torch_deterministic']
 
-    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+    device = torch.device('cuda' if torch.cuda.is_available() and args['cuda'] else 'cpu')
 
-    # env setup
+    # TODO: WANT A SINGLE BATCHABLE ENV, RATHER THAN MANY ENV INSTANCES.
+    # env setup 
     envs = gym.vector.SyncVectorEnv(
-        [make_env() for i in range(args.num_envs)],
+        [make_env() for i in range(args['num_envs'])],
     )
     assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
 
     agent = Agent(envs).to(device)
-    optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
+    optimizer = optim.Adam(agent.parameters(), lr=args['learning_rate'], eps=1e-5)
 
     # ALGO Logic: Storage setup
-    obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
-    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
-    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    obs = torch.zeros((args['num_steps'], args['num_envs']) + envs.single_observation_space.shape).to(device)
+    actions = torch.zeros((args['num_steps'], args['num_envs']) + envs.single_action_space.shape).to(device)
+    logprobs = torch.zeros((args['num_steps'], args['num_envs'])).to(device)
+    rewards = torch.zeros((args['num_steps'], args['num_envs'])).to(device)
+    dones = torch.zeros((args['num_steps'], args['num_envs'])).to(device)
+    values = torch.zeros((args['num_steps'], args['num_envs'])).to(device)
 
     # TRY NOT TO MODIFY: start the game
     global_step = 0
     start_time = time.time()
-    next_obs, _ = envs.reset(seed=args.seed)
+    next_obs, _ = envs.reset(seed=args['seed'])
     next_obs = torch.Tensor(next_obs).to(device)
-    next_done = torch.zeros(args.num_envs).to(device)
+    next_done = torch.zeros(args['num_envs']).to(device)
 
-    for iteration in range(1, args.num_iterations + 1):
+    for iteration in range(1, args['num_iterations'] + 1):
         # Annealing the rate if instructed to do so.
-        if args.anneal_lr:
-            frac = 1.0 - (iteration - 1.0) / args.num_iterations
-            lrnow = frac * args.learning_rate
+        if args['anneal_lr']:
+            frac = 1.0 - (iteration - 1.0) / args['num_iterations']
+            lrnow = frac * args['learning_rate']
             optimizer.param_groups[0]["lr"] = lrnow
 
-        for step in range(0, args.num_steps):
-            global_step += args.num_envs
+        for step in range(0, args['num_steps']):
+            global_step += args['num_envs']
             obs[step] = next_obs
             dones[step] = next_done
 
@@ -239,15 +199,15 @@ if __name__ == "__main__":
             next_value = agent.get_value(next_obs).reshape(1, -1)
             advantages = torch.zeros_like(rewards).to(device)
             lastgaelam = 0
-            for t in reversed(range(args.num_steps)):
-                if t == args.num_steps - 1:
+            for t in reversed(range(args['num_steps'])):
+                if t == args['num_steps'] - 1:
                     nextnonterminal = 1.0 - next_done
                     nextvalues = next_value
                 else:
                     nextnonterminal = 1.0 - dones[t + 1]
                     nextvalues = values[t + 1]
-                delta = rewards[t] + args.gamma * nextvalues * nextnonterminal - values[t]
-                advantages[t] = lastgaelam = delta + args.gamma * args.gae_lambda * nextnonterminal * lastgaelam
+                delta = rewards[t] + args['gamma'] * nextvalues * nextnonterminal - values[t]
+                advantages[t] = lastgaelam = delta + args['gamma'] * args['gae_lambda'] * nextnonterminal * lastgaelam
             returns = advantages + values
 
         # flatten the batch
@@ -259,12 +219,12 @@ if __name__ == "__main__":
         b_values = values.reshape(-1)
 
         # Optimizing the policy and value network
-        b_inds = np.arange(args.batch_size)
+        b_inds = np.arange(args['batch_size'])
         clipfracs = []
-        for epoch in range(args.update_epochs):
+        for epoch in range(args['update_epochs']):
             np.random.shuffle(b_inds)
-            for start in range(0, args.batch_size, args.minibatch_size):
-                end = start + args.minibatch_size
+            for start in range(0, args['batch_size'], args['minibatch_size']):
+                end = start + args['minibatch_size']
                 mb_inds = b_inds[start:end]
 
                 _, newlogprob, entropy, newvalue = agent.get_action_and_value(b_obs[mb_inds], b_actions.long()[mb_inds])
@@ -275,25 +235,25 @@ if __name__ == "__main__":
                     # calculate approx_kl http://joschu.net/blog/kl-approx.html
                     old_approx_kl = (-logratio).mean()
                     approx_kl = ((ratio - 1) - logratio).mean()
-                    clipfracs += [((ratio - 1.0).abs() > args.clip_coef).float().mean().item()]
+                    clipfracs += [((ratio - 1.0).abs() > args['clip_coef']).float().mean().item()]
 
                 mb_advantages = b_advantages[mb_inds]
-                if args.norm_adv:
+                if args['norm_adv']:
                     mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
 
                 # Policy loss
                 pg_loss1 = -mb_advantages * ratio
-                pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+                pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args['clip_coef'], 1 + args['clip_coef'])
                 pg_loss = torch.max(pg_loss1, pg_loss2).mean()
 
                 # Value loss
                 newvalue = newvalue.view(-1)
-                if args.clip_vloss:
+                if args['clip_vloss']:
                     v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
                     v_clipped = b_values[mb_inds] + torch.clamp(
                         newvalue - b_values[mb_inds],
-                        -args.clip_coef,
-                        args.clip_coef,
+                        -args['clip_coef'],
+                        args['clip_coef'],
                     )
                     v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
                     v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
@@ -302,14 +262,14 @@ if __name__ == "__main__":
                     v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
 
                 entropy_loss = entropy.mean()
-                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
+                loss = pg_loss - args['ent_coef'] * entropy_loss + v_loss * args['vf_coef']
 
                 optimizer.zero_grad()
                 loss.backward()
-                nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
+                nn.utils.clip_grad_norm_(agent.parameters(), args['max_grad_norm'])
                 optimizer.step()
 
-            if args.target_kl is not None and approx_kl > args.target_kl:
+            if args['target_kl'] is not None and approx_kl > args['target_kl']:
                 break
 
         y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
